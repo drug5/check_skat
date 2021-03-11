b894180806066fcc02fe5d365e8b7e02c8277d78
drug5 <0x2102@gmail.com>
2021-03-11 08:42:50 +0100

    fixed broken condition

--- check_skat.py
+++ check_skat.py
@@ -2,38 +2,39 @@
 # -*- coding: utf-8 -*-
 from twill.commands import *
 import twill
-from io import StringIO
-from twilio.rest import Client
 from random import randint
 import time
+from twilio.rest import Client
 
-cpr = "your_cpr_id"
-password = "your_password"
-checktxt = "2020 er tidligst klar i marts"
-message = "Aarsopgoerelse er klar!"
-notify = ['+4511111111', '+4522222222']
+text = '2020 er tidligst klar i marts'
+cpr = 'your_cpr'
+password = 'your_password'
+notify = ['+4511111111', '+4522222222', '+4533333333']
+message = 'Aarsopgoerelse er klar!'
+counter = 0
 
 while True:
-  sio = StringIO()
-  twill.set_output(sio)
-  go('https://www.tastselv.skat.dk//borger/loginpin')
-  fv("1", "pnr", cpr)
-  fv("1", "tastselvKode", password)
+  go('https://www.tastselv.skat.dk/TSADG_BORGER/loginpin.do')
+  fv('1', 'pnr', cpr)
+  fv('1', 'tastselvKode', password)
   formaction('mainForm', 'https://www.tastselv.skat.dk/TSADG_BORGER/loginpin.do')
   submit()
   go('https://www.tastselv.skat.dk/borger/seaaropg2020/VisListe.do')
   content = show()
 
-  if not checktxt in content:
+  if text in content:
+    counter = counter + 1
+    print('Ikke klar endnu - Try number '+str(counter))
+    sleep(randint(650, 850))
+  else:
+    print('Den er klar! Sender SMS')
     for sendsms in notify:
-      account_sid = 'your_twilio_account_sid'
-      auth_token = 'your_twilio_auth_token'
+      account_sid = 'your_account_sid'
+      auth_token = 'your_auth_token'
       client = Client(account_sid, auth_token)
       message = client.messages.create(
-        messaging_service_sid='your_twilio_messaging_sid',
+        messaging_service_sid='your_service_sid',
         body=message,
         to=sendsms
       )
     break
-  else:
-    sleep(randint(600,800))
